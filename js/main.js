ee5b4592948c0070fbf192a0f6e19b99289110b8
Mateus L. Aniceto <56458098+MateusAnic@users.noreply.github.com>
2021-06-20 23:25:06 -0300

    port-web

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function typeWriter(element) {
     const textArray = element.innerHTML.split('');
     element.innerHTML = '';
     textArray.forEach((letter, i) => {
-        setTimeout(() => element.innerHTML += letter, 50 * i);
+        setTimeout(() => element.innerHTML += letter, 40 * i);
     });
 }
 
