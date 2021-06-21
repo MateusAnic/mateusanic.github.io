659d2273dbc7998c46cd281209204d65a3b7b16a
Mateus L. Aniceto <56458098+MateusAnic@users.noreply.github.com>
2021-06-20 22:28:53 -0300

    portf-web

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+new SimpleAnime();
+// TypeWrite Titulo
+function typeWriter(element) {
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = '';
+    textArray.forEach((letter, i) => {
+        setTimeout(() => element.innerHTML += letter, 50 * i);
+    });
+}
+
+const titulo = document.querySelector('.title');
+typeWriter(titulo);
+
+// Loading
+
+const loader = document.querySelector(".loading");
+
+window.addEventListener('load', vanish);
+
+function vanish() {
+    loader.classList.add('disppear');
+}
+
+/* Menu Hamburguer */
+
+const btnMobile = document.querySelector('.nav-hamb');
+
+function toggleMenu() {
+    const nav = document.querySelector('.navbar');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+
+
+
+
+
